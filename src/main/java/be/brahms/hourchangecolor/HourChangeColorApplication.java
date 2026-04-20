b44c17490186adc83e696a859bbbed141268ee82
Brahms <bramso.brahms@gmail.com>
2026-04-20 19:15:58 +0200

    Create project with Spring Batch

---
+++ src/main/java/be/brahms/hourchangecolor/HourChangeColorApplication.java
@@ -0,0 +1,13 @@
+package be.brahms.hourchangecolor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourChangeColorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HourChangeColorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/brahms/hourchangecolor/HourChangeColorApplicationTests.java
@@ -0,0 +1,13 @@
+package be.brahms.hourchangecolor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourChangeColorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
