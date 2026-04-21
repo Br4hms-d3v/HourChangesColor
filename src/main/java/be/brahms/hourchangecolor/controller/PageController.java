abc0dc97413c4a63cdf041344e4a9052ef4890f8
Brahms <bramso.brahms@gmail.com>
2026-04-21 16:56:28 +0200

    This class handles the home page request and sends the current colors, message, and time to the view.

--- src/main/java/be/brahms/hourchangecolor/controller/PageController.java
+++ src/main/java/be/brahms/hourchangecolor/controller/PageController.java
@@ -8,24 +8,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller // This class handles web page requests
 public class PageController {
 
+  // Service to get colour data
   private final ColorService colorService;
 
+  // Constructor
   public PageController(ColorService colorService) {
     this.colorService = colorService;
   }
 
+  // This method handles requests to the home page "/"
   @GetMapping("/")
   public String index(Model model) {
+    // Get current time
     LocalTime localTime = LocalTime.now();
+    // Get current colour and message
     HourColorEntity HourColor = colorService.getCurrentColor();
+    // Add data to the model (sent to the view)
     model.addAttribute("backgroundColor", HourColor.getBackgroundColor());
     model.addAttribute("textColor", HourColor.getTextColor());
     model.addAttribute("message", HourColor.getMessage());
     model.addAttribute("time", localTime.truncatedTo(ChronoUnit.SECONDS).toString());
 
+    // return the page index
     return "index";
   }
 }
