24cc470d2557091497824c1a77dfd68c3f920c97
Brahms <bramso.brahms@gmail.com>
2026-04-21 17:10:04 +0200

    This class is the main entry point that starts and runs the Spring Boot application.

--- src/main/java/be/brahms/hourchangecolor/HourChangeColorApplication.java
+++ src/main/java/be/brahms/hourchangecolor/HourChangeColorApplication.java
@@ -3,9 +3,10 @@ package be.brahms.hourchangecolor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // this is the main Spring Boot application
 public class HourChangeColorApplication {
 
+  // Main method: starts the application
   public static void main(String[] args) {
     SpringApplication.run(HourChangeColorApplication.class, args);
   }
