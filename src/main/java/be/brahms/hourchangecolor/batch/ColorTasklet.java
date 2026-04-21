7410ec813e8ba4cabc02b72eec46c82b497fd3fd
Brahms <bramso.brahms@gmail.com>
2026-04-21 16:47:59 +0200

    This class runs a task that sets colors and a message based on the current time, saves them, and sends an update event.

--- src/main/java/be/brahms/hourchangecolor/batch/ColorTasklet.java
+++ src/main/java/be/brahms/hourchangecolor/batch/ColorTasklet.java
@@ -13,31 +13,37 @@ import org.springframework.batch.infrastructure.repeat.RepeatStatus;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component // This class is a Spring component (managed by Spring)
 public class ColorTasklet implements Tasklet {
 
-  private final ColorService colorService;
-  private final ApplicationEventPublisher publisher;
+  private final ColorService colorService; // Service to save colour
+  private final ApplicationEventPublisher publisher; // Used to send events
 
+  // Constructor to initialize dependencies
   public ColorTasklet(ColorService colorService, ApplicationEventPublisher publisher) {
     this.colorService = colorService;
     this.publisher = publisher;
   }
 
+  // This method is executed when the step runs
   @Override
   public @Nullable RepeatStatus execute(
-      @NonNull StepContribution contribution, @NonNull ChunkContext chunkContext) throws Exception {
+      @NonNull StepContribution contribution, @NonNull ChunkContext chunkContext) {
+
+    // Get current time
     int hour = LocalDateTime.now().getHour();
     LocalDateTime localTime = LocalDateTime.now();
 
+    // Variables for colour and message
     String backGroundColor;
     String textColor;
     String message;
 
+    // Choose colour and message based on the hour
     if (hour >= 0 && hour <= 5) {
-      backGroundColor = "#121212";
-      textColor = "#FFFFFF";
-      message = "Bonne nuit";
+      backGroundColor = "#121212"; // Colour background
+      textColor = "#FFFFFF"; // Colour of text
+      message = "Bonne nuit"; // Text display on screen
     } else if (hour <= 11) {
       backGroundColor = "#E0F7FA";
       textColor = "#000000";
@@ -52,10 +58,12 @@ public class ColorTasklet implements Tasklet {
       message = "Bonne soirée";
     }
 
+    // Save the colour and message
     colorService.saveColors(
         backGroundColor, textColor, message, localTime.truncatedTo(ChronoUnit.MINUTES));
+    // Send an event to notify that colour are update
     publisher.publishEvent(new ColorUpdatedEvent());
-
+    // End of the task
     return RepeatStatus.FINISHED;
   }
 }
